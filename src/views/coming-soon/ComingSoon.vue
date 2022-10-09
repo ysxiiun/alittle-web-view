93c107df833b2903bc3285e83528535accc4eb10
SeaSon <664876390@qq.com>
2022-10-09 22:51:26 +0800

    备案号

--- src/views/coming-soon/ComingSoon.vue
+++ src/views/coming-soon/ComingSoon.vue
@@ -3,6 +3,10 @@
     <h1>Hello, welcome ALittleShare.</h1>
     <h2>Coming soon...</h2>
   </div>
+
+  <div class="center">
+    <el-link href="https://beian.miit.gov.cn/" target="_blank">京ICP备2022017529号-1</el-link>
+  </div>
 </template>
 
 <script>
